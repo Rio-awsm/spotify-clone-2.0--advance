37ad859d1ebfe6a8d8c9f410a227d179cfe33eda
Supriyo Saha <mm.saha1982@gmail.com>
2023-08-09 17:46:11 +0530

    basic sidebar done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Figtree } from 'next/font/google'
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
 
-const font = Figtree({ subsets: ['latin'] })
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Spotifying 2.0',
-  description: 'Listen what your soul wants',
-}
+  title: "Spotifying 2.0",
+  description: "Listen what your soul wants",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
     </html>
-  )
+  );
 }
