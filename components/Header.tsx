8a9f9ca18e53161f25a30ce5b7c12b86ae19eb7e
Supriyo Saha <mm.saha1982@gmail.com>
2023-08-09 19:04:46 +0530

    complete layout done

---
+++ components/Header.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { twMerge } from "tailwind-merge";
+import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
+
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    //logout feature
+  };
+  return (
+    <div
+      className={twMerge(
+        `h-fit bg-gradient-to-b from-emerald-800 p-6`,
+        className
+      )}
+    >
+      <div className="w-full mb-4 flex items-center justify-between">
+        <div className="hidden md:flex gap-x-2 items-center">
+          <button
+            onClick={() => router.back()}
+            className="rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition"
+          >
+            <RxCaretLeft size={35} className="text-white" />
+          </button>
+          <button
+            onClick={() => router.forward()}
+            className="rounded-full 
+              bg-black 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition"
+          >
+            <RxCaretRight size={35} className="text-white" />
+          </button>
+        </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button
+            onClick={() => router.push("/")}
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button
+            onClick={() => router.push("/search")}
+            className="
+              rounded-full 
+              p-2 
+              bg-white 
+              flex 
+              items-center 
+              justify-center 
+              cursor-pointer 
+              hover:opacity-75 
+              transition
+            "
+          >
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className=" bg-transparent 
+                    text-neutral-300 
+                    font-medium"
+              >
+                Sign up
+              </Button>
+            </div>
+            <div>
+              <Button className="bg-white px-6 py-2" onClick={() => {}}>
+                Login
+              </Button>
+            </div>
+          </>
+        </div>
+      </div>
+      {children}
+    </div>
+  );
+};
+
+export default Header;
