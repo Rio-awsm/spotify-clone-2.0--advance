cb9dd121b65510ce92f5961c46adf5162817637b
Supriyo Saha <mm.saha1982@gmail.com>
2023-08-13 20:22:03 +0530

    feat::song upload

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,10 +2,21 @@
 
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const { user } = useUser();
+  const uploadModal = useUploadModal();
+
   const onClick = () => {
-    //upload later
+   if(!user) {
+    return authModal.onOpen();
+   }
+   //check for subscription
+   return uploadModal.onOpen();
   };
   return (
     <div className="flex flex-col">
