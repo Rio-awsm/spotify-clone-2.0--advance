de48eee415e05c96306482cebf745b8a362978cf
Supriyo Saha <mm.saha1982@gmail.com>
2023-08-14 01:34:16 +0530

    feat:: search added

---
+++ app/search/components/searchContent.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Song } from "@/types";
+import MediaItem from "@/components/MediaItem";
+
+interface SearchContentProps {
+  songs: Song[];
+}
+
+const searchContent: React.FC<SearchContentProps> = ({ songs }) => {
+  if (songs.length === 0) {
+    return (
+      <div
+        className="flex 
+            flex-col 
+            gap-y-2 
+            w-full 
+            px-6 
+            text-neutral-400"
+      >
+        No Songs Found 😕
+      </div>
+    );
+  }
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song: Song) => (
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <div className="flex-1">
+            <MediaItem onClick={() => {}} data={song} />
+          </div>
+          {/*add like btn*/ }
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default searchContent;
