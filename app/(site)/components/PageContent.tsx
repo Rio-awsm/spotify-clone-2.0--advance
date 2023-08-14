b49d4baeda09725dfa6b0e3fb284722fbbd184d2
Supriyo Saha <mm.saha1982@gmail.com>
2023-08-14 17:40:53 +0530

    feat::playing music

--- app/(site)/components/PageContent.tsx
+++ app/(site)/components/PageContent.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import SongItem from "@/components/SongItem";
+import useOnPlay from "@/hooks/useOnPlay";
 import { Song } from "@/types";
 
 interface PageContentProps {
   songs: Song[];
 }
 const PageContent: React.FC<PageContentProps> = ({ songs }) => {
+
+  const onPlay = useOnPlay(songs);
+
   if (songs.length === 0) {
     return <div className="mt-4 text-neutral-400">No Songs Available 😕</div>;
   }
@@ -27,7 +31,7 @@ const PageContent: React.FC<PageContentProps> = ({ songs }) => {
         {songs.map((item) => (
             <SongItem 
             key={item.id}
-            onClick = {() => {}}
+            onClick = {(id: string) => onPlay(id)}
             data={item}
             />
         ))}
